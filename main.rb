60ffa7aac055d6ba9bf61c81ab4f8950c675f142
Chris Lee <cleer@cmu.edu>
2010-11-12 02:26:29 -0500

    Restyled moar

--- main.rb
+++ main.rb
@@ -22,11 +22,12 @@ def fetch_stream(num_pages, per_page)
                    .each do |r|
       results << {
                 'url' => r.text.scan(/http:\/\/instagr.am\/p\/\S+\s*/).first,
-                'text' => r.text.split(/http:\/\/instagr.am\/p\/\S+\s*/).first,
+                'text' => r.text.split(/http:\/\/instagr.am\/p\/\S+\s*/).first.gsub('Just posted a photo', ''),
                 'photo' => get_photo(r.text.scan(/http:\/\/instagr.am\/p\/\S+\s*/).first),
                 'avatar' => r.profile_image_url,
                 'user' => r.from_user,
-                'geo' => r.geo
+                'geo' => r.geo,
+                'time' => r.created_at
                 }
     end
   end
@@ -49,6 +50,9 @@ __END__
 %html
   %head
     %title Instagramline
+    %link{:src => "http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css", :rel => "stylesheet"}
+    %link{:src => "http://fonts.googleapis.com/css?family=PT+Sans+Caption:regular,bold&subset=cyrillic", :rel => "stylesheet"}
+    %link{:src => "style.css", :rel => "stylesheet"}
     %link(rel="stylesheet" href="http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css")  
     %link(rel="stylesheet" href="http://fonts.googleapis.com/css?family=PT+Sans+Caption:regular,bold&subset=cyrillic")  
     %link(rel="stylesheet" href="style.css")
@@ -59,10 +63,12 @@ __END__
 @@ index
 - @results.each do |r|
   .result[r]
-    %img.photo{:src => r['photo']}
+    %a{:href => '/'}
+      %img.photo{:src => r['photo']}
     .info
       %img.avatar{:src => r['avatar']}
       %ul.details
-        %li.user= r['user']
+        %li.user
+          %a{:href => 'http://twitter.com/' + r['user']}= '@' + r['user']
         %li.text= r['text']
         %li.geo= r['geo']
